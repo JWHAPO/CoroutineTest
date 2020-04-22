b9c4c7e4356180cc65d2a7e5b3d901b7da256e93
JWHAPO <kim2101714@gmail.com>
2020-04-23 01:19:20 +0900

    add cancel

--- app/src/main/java/com/example/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutineexample/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.coroutineexample
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity: AppCompatActivity() {
 
@@ -14,11 +11,19 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
-        GlobalScope.launch(Dispatchers.Default){
+        //코루틴 생성
+        val job = GlobalScope.launch(Dispatchers.Default){
             repeat(10){
                 delay(1000L)
                 Log.i("TAG", "I'm Working")
             }
+        }
+
+        //3초 후 코루틴 종료
+        runBlocking {
+            delay(3000L)
+            job.cancel()
+            Log.i("TAG", "Coroutine is done")
         }
 
     }
