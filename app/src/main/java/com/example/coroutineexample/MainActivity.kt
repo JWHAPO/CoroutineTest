e3d41db9ed207d5af0c7fc78a2db1555f563f0ce
JWHAPO <kim2101714@gmail.com>
2020-04-24 01:48:49 +0900

    change something

--- app/src/main/java/com/example/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutineexample/MainActivity.kt
@@ -4,27 +4,47 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: AppCompatActivity(), CoroutineScope {
+
+    private lateinit var mJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
-        //코루틴 생성
-        val job = GlobalScope.launch(Dispatchers.Default){
-            repeat(10){
-                delay(1000L)
-                Log.i("TAG", "I'm Working")
+        mJob = Job()
+
+        launch {
+            Log.i("TAG", Thread.currentThread().name)
+            val deferred = async(Dispatchers.Default){
+                10 + 10
             }
+            Log.i("TAG", deferred.await().toString())
         }
+
+//        //코루틴 생성
+//        val job = GlobalScope.launch(Dispatchers.Default){
+//            repeat(10){
+//                delay(1000L)
+//                Log.i("TAG", "I'm Working")
+//            }
+//        }
 
         //3초 후 코루틴 종료
-        runBlocking {
-            delay(3000L)
-            job.cancel()
-            Log.i("TAG", "Coroutine is done")
-        }
+//        runBlocking {
+//            delay(3000L)
+//            job.cancel()
+//            Log.i("TAG", "Coroutine is done")
+//        }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mJob.cancel()
     }
 }
