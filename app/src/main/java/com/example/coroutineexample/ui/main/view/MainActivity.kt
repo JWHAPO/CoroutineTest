1a896fcbe316f73ac5835cdf165c1bdcf84b97ca
JWHAPO <kim2101714@gmail.com>
2020-05-04 21:33:25 +0900

    add activity

--- app/src/main/java/com/example/coroutineexample/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/coroutineexample/ui/main/view/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.coroutineexample.ui.main.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coroutineexample.R
 import com.example.coroutineexample.data.api.ApiHelper
 import com.example.coroutineexample.data.api.RetrofitBuilder
+import com.example.coroutineexample.data.model.User
 import com.example.coroutineexample.ui.base.ViewModelFactory
 import com.example.coroutineexample.ui.main.adapter.MainAdapter
 import com.example.coroutineexample.ui.main.viewmodel.MainViewModel
+import com.example.coroutineexample.utils.Status
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,15 +26,60 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupViewModel()
+        setupUI()
+        setupObservers()
 
     }
 
-    private fun setupViewModel(){
-        viewModel = ViewModelProviders.of(this, ViewModelFactory(ApiHelper(RetrofitBuilder.apiService))).get(MainViewModel::class.java)
+    private fun setupViewModel() {
+        viewModel =
+            ViewModelProviders.of(this, ViewModelFactory(ApiHelper(RetrofitBuilder.apiService)))
+                .get(MainViewModel::class.java)
     }
 
-    private fun setupUI(){
+    private fun setupUI() {
         recyclerView.layoutManager = LinearLayoutManager(this)
-
+        adapter = MainAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
     }
+
+    private fun setupObservers() {
+        viewModel.getUsers().observe(this, Observer {
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.SUCCESS -> {
+                        recyclerView.visibility = View.VISIBLE
+                        progressBar.visibility = View.GONE
+                        resource.data?.let { users -> retrieveList(users) }
+                    }
+                    Status.ERROR -> {
+                        recyclerView.visibility = View.VISIBLE
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+                        progressBar.visibility = View.VISIBLE
+                        recyclerView.visibility = View.GONE
+                    }
+                }
+
+            }
+        })
+    }
+
+    private fun retrieveList(users: List<User>) {
+        adapter.apply {
+            addUsers(users)
+            notifyDataSetChanged()
+        }
+    }
+
 }
