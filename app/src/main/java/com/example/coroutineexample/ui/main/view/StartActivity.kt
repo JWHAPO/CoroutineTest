ad9e3cdbc0fc35bfa1d53691eb42e42e5ef0cfea
JWHAPO <kim2101714@gmail.com>
2020-05-09 22:35:07 +0900

    change to no re-creation fragment in bottom navigationview

--- app/src/main/java/com/example/coroutineexample/ui/main/view/StartActivity.kt
+++ app/src/main/java/com/example/coroutineexample/ui/main/view/StartActivity.kt
@@ -3,25 +3,43 @@ package com.example.coroutineexample.ui.main.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.coroutineexample.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class StartActivity:AppCompatActivity() {
+
+    private val fragment1 : Fragment = MainFragment()
+    private val fragment2 : Fragment = Tab2Fragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragment1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            val selectedFragment: Fragment = when(it.itemId){
-                R.id.page_1 -> MainFragment()
-                R.id.page_2 -> Tab2Fragment()
-                else -> MainFragment()
-            }
+        fm.beginTransaction().add(R.id.fragment_container, fragment1, "1").hide(fragment1).commit()
+        fm.beginTransaction().add(R.id.fragment_container, fragment2, "2").hide(fragment2).commit()
 
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
-            true
+        fm.beginTransaction().hide(active).show(fragment1).commit()
+        active = fragment1
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.page_1 -> {
+                    fm.beginTransaction().hide(active).show(fragment1).commit()
+                    active = fragment1
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.page_2 -> {
+                    fm.beginTransaction().hide(active).show(fragment2).commit()
+                    active = fragment2
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
         }
 
     }
