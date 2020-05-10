d42cb07c8a9fde5c083972411a85fdaa47dd88ed
JWHAPO <kim2101714@gmail.com>
2020-05-10 21:44:14 +0900

    add taptargetview for tutorial

--- app/src/main/java/com/example/coroutineexample/ui/main/view/StartActivity.kt
+++ app/src/main/java/com/example/coroutineexample/ui/main/view/StartActivity.kt
@@ -1,10 +1,14 @@
 package com.example.coroutineexample.ui.main.view
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.coroutineexample.R
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetSequence
+import com.getkeepsafe.taptargetview.TapTargetView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class StartActivity:AppCompatActivity() {
@@ -42,5 +46,40 @@ class StartActivity:AppCompatActivity() {
             return@setOnNavigationItemSelectedListener false
         }
 
+        showTabPromptSequence()
+
+    }
+
+    private fun showTabPromptSequence(){
+        TapTargetSequence(this)
+            .targets(
+                TapTarget.forView(findViewById(R.id.page_1), "이 탭1 을 클릭하세요"),
+                TapTarget.forView(findViewById(R.id.page_2), "이 탭2를 클릭하세요.", "OK?")
+                    .dimColor(android.R.color.holo_blue_dark)
+            )
+            .listener(object: TapTargetSequence.Listener{
+                override fun onSequenceCanceled(lastTarget: TapTarget?) {
+                }
+
+                override fun onSequenceFinish() {
+                }
+
+                override fun onSequenceStep(lastTarget: TapTarget?, targetClicked: Boolean) {
+                }
+            }).start()
+    }
+
+    private fun showTabPrompt(){
+        TapTargetView.showFor(this,
+            TapTarget.forView(findViewById(R.id.page_1), "이것을 누르면 탭 1이 열립니다.")
+                .outerCircleColor(R.color.design_default_color_primary)
+                .targetRadius(60),
+            object : TapTargetView.Listener(){
+                override fun onTargetClick(view: TapTargetView) {
+                    super.onTargetClick(view)
+                    view.dismiss(true)
+                }
+            }
+        )
     }
 }
